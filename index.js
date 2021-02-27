10efbad460f00fd704fe0589475e1818c0c9fc98
Arash Saeedi <saeediarash84@gmail.com>
2021-02-27 14:41:43 -0500

    got the questions to forever loop, next stage html markdowns and combining them

--- index.js
+++ index.js
@@ -10,30 +10,24 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
+
 // let team = [];
 
-const init = () => {
-    employeeInfo()
-    .then(response => {
-        if (response.role === "Engineer") {
-            engineerEmp();
-        } else if (response.role === "Manager") {
-            managerEmp();
-        } else if (response.role === "Intern") {
-            internEmp();
-        }   newMember()
+const init = async () => {
+    await employeeInfo()
         .then(response => {
-            if(response.add_new === "Yes") {
-                employeeInfo();
-            }else if(response.add_new === "No") {
-                // generateHTML();
-                console.log("No new employees.")
-            }
-            
-        })
-    }) 
+            if (response.role === "Engineer") {
+                return engineerEmp();
+            } else if (response.role === "Manager") {
+                return managerEmp();
+            } else if (response.role === "Intern") {
+                return internEmp();
+            };
+        });
+    newMember()
 
 };
+
 
 // this will allow us to ask for user input on the emplyee
 const employeeInfo = () => {
@@ -113,19 +107,35 @@ const internEmp = () => {
 };
 
 const newMember = () => {
-    return inquirer 
-    .prompt([
-        {
-            // Employee Title
-            type: 'list',
-            message: 'Would you like to add another employee?',
-            choices: ['Yes',
-                'No',
-            ],
-            name: 'add_new',
-        },
-    ])
-}
+    return inquirer
+        .prompt([
+            {
+                // Employee Title
+                type: 'list',
+                message: 'Would you like to add another employee?',
+                choices: ['Yes',
+                    'No',
+                ],
+                name: 'add_new',
+            },
+        ]).then(response => {
+            if (response.add_new === "Yes") {
+
+               init();
+            } else if (response.add_new === "No") {
+                // generateHTML();
+                console.log("No new employees.")
+            };
+
+        });
+};
+
+init()
 
 
-init();
+
+
+
+
+
+
